ab4b2eabda99cf091d2ef897442c45b34cf87d6e
이현동 (Hyeondong Lee) <dlgusehd1211@naver.com>
2024-01-18 16:08:26 +0900

    :sparkles: Feat: 관리자용 help 메세지, 사용자용 help 메세지 분리

--- src/command/help/help.handler.ts
+++ src/command/help/help.handler.ts
@@ -2,11 +2,15 @@ import { Injectable } from '@nestjs/common';
 import { EmbedBuilder, Message } from 'discord.js';
 
 import { ICommandHandler } from '@/command/commandHandler.interface';
+import { ConfigService } from '@/config/config.service';
 import { ServerService } from '@/server/server.service';
 
 @Injectable()
 export class HelpHandler implements ICommandHandler {
-  constructor(private readonly serverService: ServerService) {}
+  constructor(
+    private readonly configService: ConfigService,
+    private readonly serverService: ServerService,
+  ) {}
 
   name = 'help';
   regex = new RegExp('^help', 'i');
@@ -16,36 +20,51 @@ export class HelpHandler implements ICommandHandler {
   }
 
   async execute(message: Message): Promise<void> {
+    const adminPrefix = this.configService.adminPrefix;
     const prefix = await this.serverService.getServerPrefix(message.guild.id);
+    const hasAdminRole = await this.serverService.hasAdminRoleGuard(
+      message,
+      message.guild,
+      message.author.id,
+    );
+    const spacer = { name: '** **', value: '\u200b' };
+
+    const adminEmbed = new EmbedBuilder()
+      .setColor('Yellow')
+      .setDescription('📝 로아크 봇 관리자용 명령어')
+      .addFields([
+        { name: '관리자 Prefix', value: `<${adminPrefix}>` },
+        spacer,
+        {
+          name: '사용자 prefix',
+          value: `<${prefix}>`,
+        },
+        spacer,
+        {
+          name: '봇 채널 설정',
+          value: `${adminPrefix} set channel`,
+        },
+        spacer,
+        { name: '봇 채널 삭제', value: `${adminPrefix} unset channel` },
+      ]);
 
     const userEmbed = new EmbedBuilder()
-      .setColor('Green')
+      .setColor('Blue')
       .setDescription('📝 로아크 봇 사용가능 명령어')
       .addFields([
         {
           name: `${prefix}help`,
           value: 'display this message',
         },
+        spacer,
         {
           name: `${prefix}ping`,
           value: 'reply `pong!`',
         },
       ]);
 
-    const adminEmbed = new EmbedBuilder()
-      .setColor('Yellow')
-      .setTitle('로아크 봇 관리자용 사용 명령어')
-      .addFields([
-        {
-          name: `${prefix}help`,
-          value: 'display this message',
-        },
-        {
-          name: `${prefix}ping`,
-          value: 'reply `pong!`',
-        },
-      ]);
-
-    await message.reply({ embeds: [userEmbed, adminEmbed] });
+    hasAdminRole
+      ? await message.reply({ embeds: [adminEmbed, userEmbed] })
+      : await message.reply({ embeds: [userEmbed] });
   }
 }
